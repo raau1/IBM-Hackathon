ece630f7cd8b867c8492903d9f6d2b193b74444f
DanStew <danielstewarts2004@gmail.com>
2025-02-08 16:02:50 +0000

    Adding updated user entity as well as Beginnings of FinanceAdvise page

--- FinanceAdviceChatbot/src/main/java/com/financeadvicechatbot/controller/MainController.java
+++ FinanceAdviceChatbot/src/main/java/com/financeadvicechatbot/controller/MainController.java
@@ -1,8 +1,22 @@
 package com.financeadvicechatbot.controller;
 
+import com.financeadvicechatbot.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class MainController {
+    @Autowired
+    private OpenAiService openAiService;
+
     @RequestMapping("/")
     public String index() { return "index";}
+
+    @RequestMapping("/financeAdvice")
+    public String mortgageAdvise(Model model) {
+
+        return "financeAdvise";
+    }
 }
--- FinanceAdviceChatbot/src/main/java/com/financeadvicechatbot/model/User.java
+++ FinanceAdviceChatbot/src/main/java/com/financeadvicechatbot/model/User.java
@@ -9,6 +9,13 @@ public class User {
     @Id
     private String email;
     private String password;
+    private String financialAim;
+    private float monthlyIncome;
+    private float monthlySavings;
+    private float spendingNeccesities;
+    private float spendingLeisure;
+    private float spendingSubscriptions;
+    private float spendingOther;
 
     // Getters and Setters
     public String getEmail() {
@@ -25,5 +32,61 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getFinancialAim() {
+        return financialAim;
+    }
+
+    public void setFinancialAim(String financialAim) {
+        this.financialAim = financialAim;
+    }
+
+    public float getMonthlyIncome() {
+        return monthlyIncome;
+    }
+
+    public void setMonthlyIncome(float monthlyIncome) {
+        this.monthlyIncome = monthlyIncome;
+    }
+
+    public float getMonthlySavings() {
+        return monthlySavings;
+    }
+
+    public void setMonthlySavings(float monthlySavings) {
+        this.monthlySavings = monthlySavings;
+    }
+
+    public float getSpendingNeccesities() {
+        return spendingNeccesities;
+    }
+
+    public void setSpendingNeccesities(float spendingNeccesities) {
+        this.spendingNeccesities = spendingNeccesities;
+    }
+
+    public float getSpendingLeisure() {
+        return spendingLeisure;
+    }
+
+    public void setSpendingLeisure(float spendingLeisure) {
+        this.spendingLeisure = spendingLeisure;
+    }
+
+    public float getSpendingSubscriptions() {
+        return spendingSubscriptions;
+    }
+
+    public void setSpendingSubscriptions(float spendingSubscriptions) {
+        this.spendingSubscriptions = spendingSubscriptions;
+    }
+
+    public float getSpendingOther() {
+        return spendingOther;
+    }
+
+    public void setSpendingOther(float spendingOther) {
+        this.spendingOther = spendingOther;
     }
 }
