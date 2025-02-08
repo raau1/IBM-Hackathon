73140d8aee40e9238832e77264caf12ec8d9b5b7
DanStew <danielstewarts2004@gmail.com>
2025-02-08 10:35:46 +0000

    Initialising Spring Boot Project

---
+++ FinanceAdviceChatbot/src/main/java/com/financeadvicechatbot/FinanceAdviceChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.financeadvicechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceAdviceChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceAdviceChatbotApplication.class, args);
+    }
+
+}
